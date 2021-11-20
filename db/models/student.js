5f2750b0e0e5a82e8a574d76af645755e13bb981
Ania Marecki <aniamarecki@gmail.com>
2021-11-20 10:19:40 -0600

    cycle 1

--- db/models/student.js
+++ db/models/student.js
@@ -1,6 +1,30 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+    firstName:{ 
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: { 
+        type: Sequelize.TEXT,
+        allowNull: false,
+       validate: {
+            isEmail: true
+       }
+    }
+});
+Student.beforeSave((student) => {
+    const nameFirst = student.firstName
+    const nameLast = student.lastName
+student.firstName = nameFirst[0].toUpperCase() + nameFirst.slice(1)
+student.lastName = nameLast[0].toUpperCase() + nameLast.slice(1)
+})
+console.log(Student.firstName)
 
 module.exports = Student;
+
